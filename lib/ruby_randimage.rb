0cc04cc9fa4f9af9f72cc080fd993128f259ff07
santiriera626 <santiriera626@gmail.com>
2016-03-06 18:47:33 +0100

    refactor

--- lib/ruby_randimage.rb
+++ lib/ruby_randimage.rb
@@ -9,79 +9,75 @@ module RubyRandimage
     background_color: "#aaaaaa"
   }
 
-  #TODO:[sriera] Optimizar
-  def self.create options={}
 
-    25.times do |i|
-      options = DEFAULT_OPTIONS.merge(options)
-      
-      width = options[:grid_size]
-      
-      #raise 'grid_size must be between 4 and 9' if options[:grid_size] < 4 || options[:grid_size] > 9
+  def self.create_and_save_file filename, options={}
 
-      svg = "<svg xmlns='http://www.w3.org/2000/svg' width='#{width}' height='#{width}'>"      
-      svg += "<title>#{options[:title]}</title>" 
-      svg += "<rect width='100%' height='100%' x='0' y='0' fill='#{options[:background_color]}' stroke='none'/>"                               
-      
-      if options[:symmetry_axes][0]
-        num_cols = (width/2.to_f).ceil
-      else
-        num_cols = width
-      end
+    # create the svg
+    svg = create(options)
 
-      if options[:symmetry_axes][1]
-        num_rows = (width/2.to_f).ceil
-      else
-        num_rows = width
-      end
+    # save to file filename
+    File.open(filename, 'wb') { |f| f.write(svg) }
 
-      (num_rows*num_cols).to_i.times do |j|
-        
-        cell = rand(num_rows*num_cols)
-        color = options[:colors][rand(options[:colors].length)]
-
-        cell_x = (cell%num_cols)
-        cell_y = (cell/num_cols)
-
-        svg += "<rect fill='#{color}' id='cell_#{cell}' stroke='none' fill-rule='nonzero' x='#{cell_x}' y='#{cell_y}' width='1' height='1' />"
-        
-        if options[:symmetry_axes][0] && width - cell_x -1 != cell_x
-          svg += "<rect fill='#{color}' id='cell_#{cell}' stroke='none' fill-rule='nonzero' x='#{width - cell_x-1}' y='#{cell_y}' width='1' height='1' />"
-          if options[:symmetry_axes][1] && width - cell_y -1 != cell_y
-            svg += "<rect fill='#{color}' id='cell_#{cell}' stroke='none' fill-rule='nonzero' x='#{width - cell_x-1}' y='#{width - cell_y-1}' width='1' height='1' />"
-          end
-        end
-
-        if options[:symmetry_axes][1] && width - cell_y -1 != cell_y
-          svg += "<rect fill='#{color}' id='cell_#{cell}' stroke='none' fill-rule='nonzero' x='#{cell_x}' y='#{width - cell_y-1}' width='1' height='1' />"
-        end
-
-      end
-      svg += "</svg>"
-      File.open("temp_image_#{i}.svg", 'wb') { |f| f.write(svg) }
-    end
   end
 
-  def self.dummy_method_to_test_coveralls options={}
-    puts DEFAULT_OPTIONS[:colors]
-    puts DEFAULT_OPTIONS[:symmetry_axes]
-    puts DEFAULT_OPTIONS[:grid_size]
-    puts DEFAULT_OPTIONS[:background_color]
-    puts DEFAULT_OPTIONS[:key]
+
+  def self.create options={}
 
     options = DEFAULT_OPTIONS.merge(options)
     
-    raise 'key is nil or less than 16 bytes' if options[:key] == nil || options[:key].length < 16
-    raise 'grid_size must be between 4 and 9' if options[:grid_size] < 4 || options[:grid_size] > 9
+    width = options[:grid_size]
+    
+    #raise 'grid_size must be between 4 and 9' if options[:grid_size] < 4 || options[:grid_size] > 9
 
-    if options[:colors] == ["#000000", "#FF0000"]
-      puts "colors default"
+    svg = "<svg xmlns='http://www.w3.org/2000/svg' width='#{width}' height='#{width}'>"      
+    svg += "<title>#{options[:title]}</title>" 
+    svg += "<rect width='100%' height='100%' x='0' y='0' fill='#{options[:background_color]}' stroke='none'/>"                               
+    
+    if options[:symmetry_axes][0]
+      num_cols = (width/2.to_f).ceil
     else
-      puts "colors not default"
-      puts "this line is not cover"
-      puts "this line is not cover too"
+      num_cols = width
     end
-    return 1
+
+    if options[:symmetry_axes][1]
+      num_rows = (width/2.to_f).ceil
+    else
+      num_rows = width
+    end
+
+    (num_rows*num_cols).to_i.times do |j|
+      
+      cell = rand(num_rows*num_cols)
+      color = options[:colors][rand(options[:colors].length)]
+
+      cell_x = (cell%num_cols)
+      cell_y = (cell/num_cols)
+
+      svg += "<rect fill='#{color}' id='cell_#{cell}' stroke='none' fill-rule='nonzero' x='#{cell_x}' y='#{cell_y}' width='1' height='1' />"
+      
+      if options[:symmetry_axes][0] && width - cell_x -1 != cell_x
+        svg += "<rect fill='#{color}' id='cell_#{cell}' stroke='none' fill-rule='nonzero' x='#{width - cell_x-1}' y='#{cell_y}' width='1' height='1' />"
+        if options[:symmetry_axes][1] && width - cell_y -1 != cell_y
+          svg += "<rect fill='#{color}' id='cell_#{cell}' stroke='none' fill-rule='nonzero' x='#{width - cell_x-1}' y='#{width - cell_y-1}' width='1' height='1' />"
+        end
+      end
+
+      if options[:symmetry_axes][1] && width - cell_y -1 != cell_y
+        svg += "<rect fill='#{color}' id='cell_#{cell}' stroke='none' fill-rule='nonzero' x='#{cell_x}' y='#{width - cell_y-1}' width='1' height='1' />"
+      end
+
+    end
+
+    svg += "</svg>"
+    return svg
+
   end
+
+  private 
+
+    def generate_matrix 
+
+    end
+
 
 end
--- spec/ruby_randimage_spec.rb
+++ spec/ruby_randimage_spec.rb
@@ -3,19 +3,14 @@ require 'spec_helper'
 describe RubyRandimage do
 
   describe 'dummy' do
-    let(:hello) { 'Hello!' }
-
-
-    it 'converts to lowercase' do
-      expect(hello).to eq "Hello!"
-    end
-
-    it "coveralls" do
-      expect(RubyRandimage.dummy_method_to_test_coveralls).to eq(1)
-    end
 
     it "text" do
-      expect(RubyRandimage.create).to eq(1)
+
+      24.times do |i|
+        RubyRandimage.create_and_save_file "temp_image_#{i}.svg"
+      end
+      expect("hola").to eq("hola")
+    
     end
 
 
