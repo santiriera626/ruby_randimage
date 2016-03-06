b44d4939fc618c8818a2bdf21dc1079116946f7e
santiriera626 <santiriera626@gmail.com>
2016-03-06 22:34:26 +0100

    cleanning

--- lib/ruby_randimage.rb
+++ lib/ruby_randimage.rb
@@ -3,14 +3,15 @@ require "ruby_randimage/version"
 
 module RubyRandimage
 
+  # TODO: [sriera] doc
   DEFAULT_OPTIONS = {
     title: "image title",
     colors: ["#aaaaaa", "#990000"],
     symmetry_axes: [true, true],
-    grid_size: 5
+    num_cells: 5
   }
 
-
+  # TODO: [sriera] doc
   def self.create_and_save_file filename, options={}
 
     # create the svg
@@ -21,19 +22,22 @@ module RubyRandimage
 
   end
 
+  # TODO: [sriera] doc
   def self.create options={}
 
     options = DEFAULT_OPTIONS.merge(options)
     
     raise "title is invalid" if options[:title] == nil || options[:title] == ''
-    raise "grid_size must be between 4 and 9" if options[:grid_size] < 4 || options[:grid_size] > 9
+    raise "num_cells must be between 4 and 9" if options[:num_cells] < 4 || options[:num_cells] > 9
 
-    matrix = generate_matrix options[:grid_size], options[:colors], options[:symmetry_axes] 
-    return generate_svg options[:title], options[:grid_size], matrix
+    matrix = generate_matrix options[:num_cells], options[:colors], options[:symmetry_axes]
+
+    return generate_svg options[:title], matrix
   end
 
-  private 
+  private
 
+    # TODO: [sriera] doc
     def self.generate_matrix num_cells, colors, symmetry_axes
       
       cells = Array.new
@@ -69,17 +73,27 @@ module RubyRandimage
       return cells
     end
 
-    def self.generate_svg title, grid_size, matrix
-      puts "#{matrix.each{|l| puts l.join(',')}}"
-      svg = "<svg xmlns='http://www.w3.org/2000/svg' width='#{grid_size}' height='#{grid_size}'>"      
+    # TODO: [sriera] doc
+    def self.generate_svg title, matrix
+
+      # open svg tag
+      size = matrix.length
+      svg = "<svg xmlns='http://www.w3.org/2000/svg' width='#{size}' height='#{size}'>"
+
+      # set title
       svg += "<title>#{title}</title>" 
-      svg += "<rect width='100%' height='100%' x='0' y='0' fill='#{matrix[0][0]}' stroke='none'/>"   
-      grid_size.times do |col|
-        grid_size.times do |row|          
+
+      # background with first color
+      svg += "<rect width='100%' height='100%' x='0' y='0' fill='#{matrix[0][0]}' stroke='none'/>"
+
+      size.times do |col|
+        size.times do |row|
+          # draw rect if its color its different than background color
           svg += "<rect fill='#{matrix[col][row]}' stroke='none' fill-rule='nonzero' x='#{col}' y='#{row}' width='1' height='1' />" if matrix[col][row] != matrix[0][0]
         end
       end 
 
+      # close svg tag
       svg += "</svg>"
 
     end
--- spec/ruby_randimage_spec.rb
+++ spec/ruby_randimage_spec.rb
@@ -2,34 +2,28 @@ require 'spec_helper'
 
 describe RubyRandimage do
   
-  before(:all) do
-    @@blob = []
-    @@result = []
-  end
-
   describe 'dummy' do
 
     # TODO: [sriera] Eliminar antes de build
     it "text" do
       25.times do |i|
-        RubyRandimage.create_and_save_file "temp_image_#{i}.svg" , { :title=>"image_#{i}", :colors=> ["#aaaaaa", "#990000"], symmetry_axes: [true, true], grid_size: 5}
+        RubyRandimage.create_and_save_file "temp_image_#{i}.svg" , { :title=>"image_#{i}", :colors=> ["#aaaaaa", "#990000"], symmetry_axes: [true, true], num_cells: 5}
       end
-    
     end
 
     it "creates a svg file" do
-      @@blob = RubyRandimage.create
-      expect(@@blob).to be_a_kind_of(String)
+      blob = RubyRandimage.create
+      expect(blob).to be_a_kind_of(String)
     end
 
     it "creates an image file" do
-      @@result = RubyRandimage.create_and_save_file("tmp/test_image.svg")
-      expect(@@result).to be_truthy
+      result = RubyRandimage.create_and_save_file("tmp/test_image.svg")
+      expect(result).to be_truthy
     end
 
     it "does not create a svg image with an invalid grid size" do
-      lambda { RubyRandimage.create(grid_size: 2) }.should raise_exception('grid_size must be between 4 and 9')
-      lambda { RubyRandimage.create(grid_size: 10) }.should raise_exception('grid_size must be between 4 and 9')
+      lambda { RubyRandimage.create(num_cells: 2) }.should raise_exception('num_cells must be between 4 and 9')
+      lambda { RubyRandimage.create(num_cells: 10) }.should raise_exception('num_cells must be between 4 and 9')
     end
 
     it "does not create a svg image with an invalid title" do
