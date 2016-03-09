b6de8ae1917451f8566a5f7ec0d0baac648e21bc
santiriera626 <santiriera626@gmail.com>
2016-03-09 15:43:31 +0100

    antialiasing off

--- lib/ruby_randimage.rb
+++ lib/ruby_randimage.rb
@@ -78,18 +78,18 @@ module RubyRandimage
 
       # open svg tag
       size = matrix.length
-      svg = "<svg xmlns='http://www.w3.org/2000/svg' width='#{size}' height='#{size}'>"
+      svg = "<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='#{size}' height='#{size}'>"
 
       # set title
       svg << "<title>#{title}</title>" 
 
       # background with first color
-      svg << "<rect width='100%' height='100%' x='0' y='0' fill='#{matrix[0][0]}' stroke='none'/>"
+      svg << "<rect width='100%' height='100%' x='0' y='0' fill='#{matrix[0][0]}' stroke='none' shape-rendering='crispEdges'/>"
 
       size.times do |col|
         size.times do |row|
           # draw rect if its color its different than background color
-          svg << "<rect fill='#{matrix[col][row]}' stroke='none' fill-rule='nonzero' x='#{col}' y='#{row}' width='1' height='1' />" if matrix[col][row] != matrix[0][0]
+          svg << "<rect fill='#{matrix[col][row]}' stroke='none' fill-rule='nonzero' x='#{col}' y='#{row}' width='1' height='1' shape-rendering='crispEdges' />" if matrix[col][row] != matrix[0][0]
         end
       end 
 
