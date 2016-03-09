1b85b37caf6521f30b3b619d9103cb34d5117162
santiriera626 <santiriera626@gmail.com>
2016-03-09 01:07:53 +0100

    coveralls is broken, gem is ok

--- spec/ruby_randimage_spec.rb
+++ spec/ruby_randimage_spec.rb
@@ -5,41 +5,41 @@ describe RubyRandimage do
   describe 'dummy' do
 
     # TODO: [sriera] Eliminar antes de build
-    #it "text" do
-    #  25.times do |i|
-    #    RubyRandimage.create_and_save_file "temp_image_#{i}.svg" , { :title=>"image_#{i}", :colors=> ["#ffffff","#ffffff","#ffffff", "#770000", "#550000", "#990000", "#990000", "#990000"], :symmetry_axes=> [true, true], :num_cells=> 10}
-    #  end
-    #end
+    it "text" do
+      25.times do |i|
+        RubyRandimage.create_and_save_file "temp_image_#{i}.svg" , { :title=>"image_#{i}", :colors=> ["#ffffff","#ffffff","#ffffff", "#770000", "#550000", "#990000", "#990000", "#990000"], :symmetry_axes=> [true, true], :num_cells=> 10}
+      end
+    end
 
     it "creates a svg" do
-      svg = RubyRandimage.create
-      expect(svg).to be_a_kind_of(String)
+      svg_string = RubyRandimage.create
+      expect(svg_string).to be_a_kind_of(String)
     end
 
     it "creates a svg without simetry" do
-      svg = RubyRandimage.create({ :symmetry_axes=> [false, false] })
-      expect(svg).to be_a_kind_of(String)
+      svg_string = RubyRandimage.create({ :symmetry_axes=> [false, false] })
+      expect(svg_string).to be_a_kind_of(String)
     end
 
     it "creates a svg with horizontal simetry" do
-      svg = RubyRandimage.create({ :symmetry_axes=> [true, false] })
-      expect(svg).to be_a_kind_of(String)
+      svg_string = RubyRandimage.create({ :symmetry_axes=> [true, false] })
+      expect(svg_string).to be_a_kind_of(String)
     end
 
     it "creates a svg with vertical simetry" do
-      svg = RubyRandimage.create({ :symmetry_axes=> [false, true] })
-      expect(svg).to be_a_kind_of(String)
+      svg_string = RubyRandimage.create({ :symmetry_axes=> [false, true] })
+      expect(svg_string).to be_a_kind_of(String)
     end
 
     it "creates a svg with horizontal and vertical simetry" do
-      svg = RubyRandimage.create({ :symmetry_axes=> [false, true] })
-      expect(svg).to be_a_kind_of(String)
+      svg_string = RubyRandimage.create({ :symmetry_axes=> [false, true] })
+      expect(svg_string).to be_a_kind_of(String)
     end
 
-    #it "creates an svg file" do
-    #  result = RubyRandimage.create_and_save_file("test_image_0.svg")
-    #  expect(result).to be_truthy
-    #end
+    it "creates an svg file" do
+      result = RubyRandimage.create_and_save_file("tmp/test_image.svg")
+      expect(result).to be_truthy
+    end
 
     it "does not create a svg image with an invalid grid size" do
       lambda { RubyRandimage.create(:num_cells=> 1) }.should raise_exception('num_cells must be between 2 and 256')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
-require 'ruby_randimage'
-
 require 'coveralls'
+require 'ruby_randimage'
 Coveralls.wear!
 
+RSpec.configure do |config|
+
+  config.before(:all) do
+    Dir.mkdir('tmp') unless File.directory?('tmp')
+  end
+
+  config.after(:all) do
+    FileUtils.rm_rf("tmp")
+  end
+end
